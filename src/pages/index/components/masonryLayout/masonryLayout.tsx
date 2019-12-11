602f36053b478be96deff085d499ac3f9cdcac3a
kyuuube <benchiu@yandex.com>
2019-12-11 18:02:46 +0800

    mobx、 瀑布流样式调整、又后端提供扩展数据

--- src/pages/index/components/masonryLayout/masonryLayout.tsx
+++ src/pages/index/components/masonryLayout/masonryLayout.tsx
@@ -1,6 +1,7 @@
 import Taro, { Component } from '@tarojs/taro'
+import { observable } from 'mobx';
 // import classnames from 'classnames'
-import { View, Image } from '@tarojs/components'
+import { View } from '@tarojs/components'
 
 import './index.scss'
 
@@ -10,35 +11,156 @@ type Props = {
 }
 
 type State = {
-  imgSrc1: string
-  imgSrc2: string
+  // imgSrc1: string
+  // imgSrc2: string
+  list: any[],
+  // images: any[],
+  heightArr: any[]
 }
 
 export default class MasonryLayout extends Component<Props, State> {
-
+  @observable images
   constructor (props) {
     super(props)
     this.state = {
-      imgSrc1: require('../../../../assets/temp/001.png'),
-      imgSrc2: require('../../../../assets/temp/008.png')
+      // imgSrc1: require('../../../../assets/temp/001.png'),
+      // imgSrc2: require('../../../../assets/temp/008.png'),
+      // images: [{
+      //   width: 360,
+      //   height: 540
+      // }, {
+      //   width: 480,
+      //   height: 540
+      // }, {
+      //   width: 540,
+      //   height: 720
+      // }, {
+      //   width: 720,
+      //   height: 960
+      // }, {
+      //   width: 540,
+      //   height: 960
+      // }, {
+      //   width: 360,
+      //   height: 720
+      // }, {
+      //   width: 360,
+      //   height: 960
+      // }, {
+      //   width: 540,
+      //   height: 540
+      // }, {
+      //   width: 540,
+      //   height: 1440
+      // }, {
+      //   width: 960,
+      //   height: 1440
+      // }],
+      list: [],
+      heightArr: []
     }
   }
 
   componentWillMount() {
+    this.images = [{
+        width: 360,
+        height: 540
+      }, {
+        width: 480,
+        height: 540
+      }, {
+        width: 540,
+        height: 720
+      }, {
+        width: 720,
+        height: 960
+      }, {
+        width: 540,
+        height: 960
+      }, {
+        width: 360,
+        height: 720
+      }, {
+        width: 360,
+        height: 960
+      }, {
+        width: 540,
+        height: 540
+      }, {
+        width: 540,
+        height: 1440
+      }, {
+        width: 960,
+        height: 1440
+      }]
+    this.initData(2)
+  }
+
+  setDataList() {
+    this.images = [...this.images, ...this.images]
+    this.initData(2)
+  }
+
+  initData (col) {
+    let images: any[] = []
+    let list: any[] = []
+    let heightArr: any[] = this.state.heightArr
+    // 模拟图片宽高
+    // for (let i = 0; i < 10; i++) {
+      // let image = this.state.images[Math.floor(Math.random() * 10)]
+    //   images.push({...image, id: i})
+    // }
+    images = this.images.map((i, index) => ({...i, id: index}))
+    for (let i in images) {
+      const scale = 182.5 / images[i].width
+      let height = images[i].height * scale
+      // console.log(scale)
+      images[i].showHeight = height
+      images[i].showWidth = 182.5
+      console.log('preRendingList' , list)
+      // 第一行的两个盒子
+      if (i < col) {
+        console.log('preRendingList if col > i' , list)
+        list.push([images[i]])
+        heightArr.push(height)
+      } else {
+        // 选出高度较矮的一列的索引
+        let minHeight = Math.min.apply(null, this.state.heightArr)
+        console.log(minHeight)
+        let minHeightIndex = this.state.heightArr.indexOf(minHeight)
+        console.log('target list', this.state.heightArr.indexOf(minHeight))
+        list[minHeightIndex].push(images[i])
+        heightArr = this.state.heightArr
+        heightArr[minHeightIndex] += height
+        this.setState({heightArr: heightArr})
+      }
+    }
+    this.setState({
+      list: list
+    })
+    this.setState({heightArr: []})
   }
 
   render() {
-    const { imgSrc1, imgSrc2 } = this.state
+    const { list } = this.state
+    console.log('rendering', list)
     return (
-        <View className='masonry-layout'>
+      <View className='masonry-layout' onClick={this.setDataList}>
           {
-            Array(20).fill('').map((item, index) => {
+            list.map((item, index) => {
               return (
-                <View key={index} className='item'>
-                      <Image src={index%2 === 0 ?  imgSrc1 : imgSrc2} />
-                  <View>
-                    {item}
-                  </View>
+                <View key={index} className='column'>
+                  {
+                    item.map((i) => {
+                      return (
+                        <View key={i.id} className='item' style={`height: ${i.showHeight}rpx; width: ${i.showWidth}PX`}>
+                          <View>
+                            {i.id}
+                          </View>
+                        </View>
+                      )
+                    })
+                  }
                 </View>
               )
             })
--- src/pages/index/components/recommend/recommend.tsx
+++ src/pages/index/components/recommend/recommend.tsx
@@ -50,42 +50,42 @@ export default class Recommend extends Component<Props, State> {
     const ActiveClass = classnames({'recommend-swi-warp': true, 'second-swi': currentTab === 1})
     return (
         <View className='recommend'>
-          <View className='hot-topic'>热门话题</View>
-          <Swiper
-            onChange={(e:any) => this.changeCurrent(e.currentTarget.current)}
-            current={currentTab}
-            className={ActiveClass}
-            indicatorColor='#999'
-            indicatorActiveColor='#333'
-            circular
-            indicatorDots
-          >
-            <SwiperItem>
-              <View className='recommend-swi-item'>
-                {
-                  Array(4).fill({topic: "爆气侧漏填填填"}).map((i, index) => {
-                    return <View key={index} className='topic-item at-row'>
-                      <View className='topic-image at-col at-col-4'>
-                        <Image src='https://i.loli.net/2019/11/20/wQfDT1cBZ2yivtz.jpg' />
+            <View className='hot-topic'>热门话题</View>
+            <Swiper
+              onChange={(e:any) => this.changeCurrent(e.currentTarget.current)}
+              current={currentTab}
+              className={ActiveClass}
+              indicatorColor='#999'
+              indicatorActiveColor='#333'
+              circular
+              indicatorDots
+            >
+              <SwiperItem>
+                <View className='recommend-swi-item'>
+                  {
+                    Array(4).fill({topic: "爆气侧漏填填填"}).map((i, index) => {
+                      return <View key={index} className='topic-item at-row'>
+                        <View className='topic-image at-col at-col-4'>
+                          <Image src='https://i.loli.net/2019/11/20/wQfDT1cBZ2yivtz.jpg' />
+                        </View>
+                        <View className='topic-info at-col'>
+                          <View className='topic-title'>{`#${i.topic}`}</View>
+                          <View className='topic-number'>233条内容</View>
+                        </View>
                       </View>
-                      <View className='topic-info at-col'>
-                        <View className='topic-title'>{`#${i.topic}`}</View>
-                        <View className='topic-number'>233条内容</View>
-                      </View>
-                    </View>
-                  })
-                }
-              </View>
-            </SwiperItem>
-            <SwiperItem>
-              <View className='recommend-swi-item'>
-                {
-                  this.renderTopicItem()
-                }
-              </View>
-            </SwiperItem>
-          </Swiper>
-          <MasonryLayout />
+                    })
+                  }
+                </View>
+              </SwiperItem>
+              <SwiperItem>
+                <View className='recommend-swi-item'>
+                  {
+                    this.renderTopicItem()
+                  }
+                </View>
+              </SwiperItem>
+            </Swiper>
+            <MasonryLayout />
         </View>
     )
   }
