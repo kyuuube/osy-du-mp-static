ba252433a1c1a06faf2d83b0b8caab7c58d7728c
kyuuube <benchiu@yandex.com>
2020-05-09 09:53:02 +0800

    升级2.0

--- config/index.js
+++ config/index.js
@@ -1,103 +1,64 @@
-'use strict';
-
-var config = {
-  projectName: 'oyster-mp-v2-static',
-  date: '2019-11-21',
+const config = {
+  projectName: 'taro-framework',
+  date: '2019-11-2',
   designWidth: 750,
   deviceRatio: {
-    '640': 1.17,
-    '750': 1,
-    '828': 0.905
+    640: 2.34 / 2,
+    750: 1,
+    828: 1.81 / 2
   },
   sourceRoot: 'src',
   outputRoot: 'dist',
-  plugins: {
-    babel: {
-      sourceMap: true,
-      presets: [
-        [
-          'env',
-          {
-            modules: false
-          }
-        ]
-      ],
-      plugins: [
-        'transform-decorators-legacy',
-        'transform-class-properties',
-        'transform-object-rest-spread',
-        [
-          'transform-runtime',
-          {
-            helpers: false,
-            polyfill: false,
-            regenerator: true,
-            moduleName: 'babel-runtime'
-          }
-        ]
-      ]
-    }
+  // babel、csso、uglify 等配置从 plugins 配置中移出来
+  babel: {
+    sourceMap: true,
+    presets: [['env', { modules: false }]],
+    plugins: [
+      'transform-decorators-legacy',
+      'transform-class-properties',
+      'transform-object-rest-spread'
+    ]
   },
-  defineConstants: {},
-  copy: {
-    patterns: [],
-    options: {}
-  },
-  weapp: {
-    module: {
-      postcss: {
-        autoprefixer: {
-          enable: true,
-          config: {
-            browsers: ['last 3 versions', 'Android >= 4.1', 'ios >= 8']
-          }
-        },
-        pxtransform: {
-          enable: true,
-          config: {}
-        },
-        url: {
-          enable: true,
-          config: {
-            limit: 10240 // 设定转换尺寸上限
-          }
-        },
-        cssModules: {
-          enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
-          config: {
-            namingPattern: 'module', // 转换模式，取值为 global/module
-            generateScopedName: '[name]__[local]___[hash:base64:5]'
-          }
+  // 小程序配置从 weapp 改为 mini，可以删掉很多小配置
+  mini: {
+    webpackChain (chain, webpack) {},
+    cssLoaderOption: {},
+    postcss: {
+      pxtransform: {
+        enable: true,
+        config: {}
+      },
+      url: {
+        enable: true,
+        config: {
+          limit: 10240 // 设定转换尺寸上限
         }
       }
     }
   },
+  // 可以删掉很多小配置
   h5: {
     publicPath: '/',
     staticDirectory: 'static',
-    module: {
-      postcss: {
-        autoprefixer: {
-          enable: true,
-          config: {
-            browsers: ['last 3 versions', 'Android >= 4.1', 'ios >= 8']
-          }
-        },
-        cssModules: {
-          enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
-          config: {
-            namingPattern: 'module', // 转换模式，取值为 global/module
-            generateScopedName: '[name]__[local]___[hash:base64:5]'
-          }
+    webpackChain (chain, webpack) {},
+    postcss: {
+      autoprefixer: {
+        enable: true,
+        config: {
+          browsers: [
+            'last 3 versions',
+            'Android >= 4.1',
+            'ios >= 8'
+          ]
         }
       }
     }
   }
-};
+}
 
-module.exports = function(merge) {
-  {
-    return merge({}, config, require('./dev.js'));
+module.exports = function (merge) {
+  if (process.env.NODE_ENV === 'development') {
+    return merge({}, config, require('./dev'))
   }
-  return merge({}, config, require('./prod.js'));
-};
+  return merge({}, config, require('./prod'))
+}
